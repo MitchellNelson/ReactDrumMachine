c1180b728b18084ebb7f676db0028e7dc187ee8f
Mitchell Nelson <nels4929@stthomas.edu>
2020-11-14 08:56:14 -0600

    Updates

---
+++ precache-manifest.0d6f9b4602f7db9f3154880237ec0ae8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7752c542bb251d9575631a9693951974",
+    "url": "/ReactDrumMachine/index.html"
+  },
+  {
+    "revision": "e825929861e94442e82e",
+    "url": "/ReactDrumMachine/static/css/main.2b9e9f7c.chunk.css"
+  },
+  {
+    "revision": "b6f2d0899b2765829a45",
+    "url": "/ReactDrumMachine/static/js/2.3af47d79.chunk.js"
+  },
+  {
+    "revision": "b399bb2b2ea08630cb75b076e919735c",
+    "url": "/ReactDrumMachine/static/js/2.3af47d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e825929861e94442e82e",
+    "url": "/ReactDrumMachine/static/js/main.7ec82059.chunk.js"
+  },
+  {
+    "revision": "3b96d271dde7ab066efc",
+    "url": "/ReactDrumMachine/static/js/runtime-main.6bfe0194.js"
+  },
+  {
+    "revision": "45bf1df81152e3eea46ebc1231fa3084",
+    "url": "/ReactDrumMachine/static/media/Clap1.45bf1df8.wav"
+  },
+  {
+    "revision": "2c3bd2ef6316a21607e04e2fec3adb1d",
+    "url": "/ReactDrumMachine/static/media/Hi Hat1.2c3bd2ef.wav"
+  },
+  {
+    "revision": "1dfc137c1dd35d68686d366868e03d83",
+    "url": "/ReactDrumMachine/static/media/Kick1.1dfc137c.wav"
+  },
+  {
+    "revision": "85a0276daf084f39a331e9405b51ea3a",
+    "url": "/ReactDrumMachine/static/media/Open Hihat.85a0276d.mp3"
+  },
+  {
+    "revision": "f90bc65c7675f2951a18b98592b4a3a1",
+    "url": "/ReactDrumMachine/static/media/Snare1.f90bc65c.wav"
+  },
+  {
+    "revision": "a21c211d9a231614364e2fec44020d38",
+    "url": "/ReactDrumMachine/static/media/Tamberine1.a21c211d.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactdrummachine/precache-manifest.c735dd4344f4e253647a9deae0c7c39a.js"
+  "/ReactDrumMachine/precache-manifest.0d6f9b4602f7db9f3154880237ec0ae8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactdrummachine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactDrumMachine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
